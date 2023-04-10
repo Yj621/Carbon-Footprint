ec3ee4c346ba5e82cde78379897027c5259128b3
Yj621 <wjddbswl26@gmail.com>
2023-04-11 00:39:46 +0900

    test
    test

---
+++ Carbon-footprint/Assets/Code/InfoPanelController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InfoPanelController : MonoBehaviour
+{
+    public GameObject RoomInfoPanel;
+    public GameObject RoomInfoButton;
+
+    void Start()
+    {
+        RoomInfoPanel.SetActive(false);
+
+        //ready 버튼이 눌리면 RoomInfoButtonClicked 활성화
+        Button roomInfoBtn = GameObject.Find("RoomInfoBtn").GetComponent<Button>();
+        roomInfoBtn.onClick.AddListener(RoomInfoButtonClicked);
+
+        Button readyBtn = GameObject.Find("ReadyBtn").GetComponent<Button>();
+        readyBtn.onClick.AddListener(OnReadyButtonClicked);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            RoomInfoPanel.SetActive(false);
+        }
+    }
+
+    public void RoomInfoButtonClicked()
+    {
+        RoomInfoPanel.SetActive(true);
+    }
+
+    public void OnReadyButtonClicked()
+    {
+        SceneManager.LoadScene("GamePlaySene");
+        //여기 오타났음...ㅎㅎ^,^;
+    }
+}
+
